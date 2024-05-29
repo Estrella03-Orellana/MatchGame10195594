03e07aa5d9de61e3c563edf997abf3f766ab5668
Estrella <Estrella@MELI>
2024-05-29 17:22:57 -0600

    Agregar archivos de proyecto.

---
+++ MatchGame10195594/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿namespace MatchGame10195594
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            SetUpGame();
+        }
+        private void SetUpGame()
+        {
+            List<string> animalEmoji = new List<string>()
+            { "💟","💟",
+                "🎁","🎁",
+                " ⚓","⚓",
+                "⭐","⭐",
+                "🎀","🎀 ",
+                " 🖼️","🖼️",
+                "💋 ","💋 ",
+                "🎸","🎸 "
+            };
+            Random random = new Random();
+            foreach (Button view in Grid1.Children)
+            {
+                int index = random.Next(animalEmoji.Count);
+                string nextEmoji = animalEmoji[index];
+                view.Text = nextEmoji;
+                animalEmoji.RemoveAt(index);
+            }
+        }
+
+        Button ultimoButtonClicked;
+        bool encontrandoMatch = false;
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Button button = sender as Button;
+            if (encontrandoMatch == false)
+            {
+                button.IsVisible = false;
+                ultimoButtonClicked = button;
+                encontrandoMatch = true;
+            }
+            else if (button.Text == ultimoButtonClicked.Text)
+            {
+                button.IsVisible = false;
+                encontrandoMatch = false;
+            }
+            else 
+            {
+                ultimoButtonClicked.IsVisible = true;
+                encontrandoMatch = false;
+            }
+        }
+
+    }
+
+}
